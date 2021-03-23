fe482c2624eef7fa1c797847a9b421c03a5d4cb7
Sergeytsev A <Sergeytsev A>
2021-03-23 17:44:35 +0300

    два из трёх работают

--- LessonOnePartTwo/ViewController.swift
+++ LessonOnePartTwo/ViewController.swift
@@ -20,27 +20,64 @@ class ViewController: UIViewController {
         
         startButton.layer.cornerRadius = 10
        
-        redColorView.layer.cornerRadius = 40
         redColorView.alpha = 0.3
-        yellowColorView.layer.cornerRadius = 40
+        redColorView.layer.cornerRadius = redColorView.frame.size.width / 2
+        redColorView.clipsToBounds = true
+        
         yellowColorView.alpha = 0.3
-        greenColorView.layer.cornerRadius = 40
+        yellowColorView.layer.cornerRadius = redColorView.frame.size.width / 2
+        yellowColorView.clipsToBounds = true
+        
+        
         greenColorView.alpha = 0.3
+        greenColorView.layer.cornerRadius = redColorView.frame.size.width / 2
+        greenColorView.clipsToBounds = true
     }
 
 
     @IBAction func startButtonClick() {
         startButton.setTitle("NEXT", for: .normal)
         
+        var redOnSwitch = false
+        var yellowOnSwitch = false
+//        var greenOnSwitch = false
+        
         if redColorView.alpha != 1 {
             redColorView.alpha = 1
-        } else if redColorView.alpha == 1 {
+            redOnSwitch = true
+            print (redOnSwitch)
+        } else {
             redColorView.alpha = 0.3
+            redOnSwitch = false
+            print (redOnSwitch)
         }
+        
+        if yellowColorView.alpha != 1 && redOnSwitch == false {
+            yellowColorView.alpha = 1
+            yellowOnSwitch = true
+            print (yellowOnSwitch)
+        } else {
+            yellowColorView.alpha = 0.3
+            yellowOnSwitch = false
+            print (yellowOnSwitch)
+        }
+        
+//        if greenColorView.alpha != 1 && yellowOnSwitch == false && greenOnSwitch == false {
+//            greenColorView.alpha = 1
+//            greenOnSwitch = true
+//            print (greenOnSwitch)
+//        } else {
+//            greenColorView.alpha = 0.3
+//            greenOnSwitch = false
+//            print (greenOnSwitch)
+//        }
+ 
+        
+        
         
         
     }
     
-
+    
 }
 
