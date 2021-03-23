39441aebe4726dae0370403f6cf7ed3ccafec035
Sergeytsev A <Sergeytsev A>
2021-03-23 14:52:07 +0300

    сделал включение/выключение красного

--- LessonOnePartTwo/ViewController.swift
+++ LessonOnePartTwo/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redColorView: UIView!
+    @IBOutlet weak var yellowColorView: UIView!
+    @IBOutlet weak var greenColorView: UIView!
+  
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+       
+        redColorView.layer.cornerRadius = 40
+        redColorView.alpha = 0.3
+        yellowColorView.layer.cornerRadius = 40
+        yellowColorView.alpha = 0.3
+        greenColorView.layer.cornerRadius = 40
+        greenColorView.alpha = 0.3
     }
 
+
+    @IBAction func startButtonClick() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        if redColorView.alpha != 1 {
+            redColorView.alpha = 1
+        } else if redColorView.alpha == 1 {
+            redColorView.alpha = 0.3
+        }
+        
+        
+    }
+    
 
 }
 
